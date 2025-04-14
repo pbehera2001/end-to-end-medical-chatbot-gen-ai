c02e0df824a79cbf9f5e1a26246e9492a0c12593
pbehera2001 <beherapriya090@gmail.com>
2025-04-15 00:29:23 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Priya Behera',
+    author_email= 'beherapriya080@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
